588cf97a41d4ff5bf239ff173329e1085aa98cc8
vaddadi-abhishek <abhiabhishek2801@gmail.com>
2024-08-21 18:55:36 +0530

    project changes

--- src/App.js
+++ src/App.js
@@ -3,24 +3,40 @@ import Home from "./templates/home";
 import Education from "./templates/education";
 import Skills from "./templates/skills";
 import { Fragment } from "react";
-import "./css/app.css"
+import './app.css';
 import Certification from "./templates/certification";
 import Work from "./templates/work";
 import Projects from "./templates/project";
 
 function App() {
   return (
-    <Fragment>
-      <SideNav/>
-      <div className="app-content bg-dark text-light" data-bs-spy="scroll" data-bs-target="#NavLinks" tabindex="0">
-        <Home />
-        <Education />
-        <Skills />
-        <Work />
-        <Projects />
-        <Certification />
+    // <Fragment>
+    //   <SideNav/>
+    //   <div className="app-content bg-dark text-light" data-bs-spy="scroll" data-bs-target="#NavLinks" tabindex="0">
+    //     <Home />
+    //     <Education />
+    //     <Skills />
+    //     <Work />
+    //     <Projects />
+    //     <Certification />
+    //   </div>
+    // </Fragment>
+
+    <div className="row text-center">
+      <div className="col-md-3">
+        <SideNav />
       </div>
-    </Fragment>
+      <div className="col-md-9">
+        <div className="app-content bg-dark text-light" data-bs-spy="scroll" data-bs-target="#NavLinks" tabindex="0">
+          <Home />
+          <Education />
+          <Skills />
+          <Work />
+          <Projects />
+          <Certification />
+        </div>
+      </div>
+    </div>
   );
 }
 
